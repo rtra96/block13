97ccd658211ab7e55ccb84d01fae4eba5ff46955
Rick Trahant <rtra96@gmail.com>
2023-09-23 15:49:11 -0500

    greater than 5 and IS TRUTHY complete. started making progress on number line

---
+++ index.js
@@ -0,0 +1,61 @@
+//is truthy
+
+
+
+// let value = true;
+
+// if (value==true){
+//     console.log ("true");
+// }
+
+// if (value === false){
+//     console.log ("the boolean value is falsey")
+// }
+
+// if (value === null){
+//     console.log ("the null value is falsey")
+// }
+
+// if (value === undefined){
+//     console.log ("undefined is falsey")
+// }
+
+// if (value === 0){
+//     console.log ("The number 0 is falsy (the only falsy number)")
+// }
+//  if (value === ""){
+//     console.log ("The empty string is falsy (the only falsy string)");
+//  }
+ 
+
+// Number Line
+let num1 = 99
+let num2 = -2
+
+if (num1 + num2 > 100) {
+    console.log (num1 + num2);
+    console.log( "is greater than 100");
+   
+}
+if (num1 + num2 > 0) {
+    console.log (num1+ num2);
+    console.log  (" is greater than 0")
+}
+
+
+
+
+
+
+
+// Greater Than 5
+
+// let a = 5
+// let b = 5
+// if (a && b >= 5) {
+//     console.log ("true")
+// }
+// else {
+//     console.log ("false")
+// }
+
